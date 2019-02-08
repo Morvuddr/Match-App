3ad0eb38f098f65e4b6039a6c22cb8b195465f88
Igor Bopp <igor_bopp@mail.ru>
2019-02-08 13:22:09 +0300

    Added protocols and delegates

--- Match App/ViewController.swift
+++ Match App/ViewController.swift
@@ -8,20 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
+    @IBOutlet weak var collectionView: UICollectionView!
     var model = CardModel()
     var cardArray = [Card]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
         // Call the getCards method of the card model
         cardArray = model.getCards()
         
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        
+        
+    }
+    
+    // MARK: - UICollectionView Protocol Methods
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cardArray.count
     }
 
-
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath)
+        
+        return cell
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+    
 }
 
